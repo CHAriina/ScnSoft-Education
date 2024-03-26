33899c15d1eadc08ff12bbe18e17aadea8402f9d
Arina Charniauskaya <acharniauskaya@scnsoft.com>
2024-03-26 12:03:52 +0300

    The solution for two sum problem

---
+++ ScnSoft-Education/LeetCodeTraining.Tests/TwoSumSolutionTests.cs
@@ -0,0 +1,25 @@
+﻿using Xunit;
+
+using LeetCodeTraining.TwoSum1;
+
+namespace LeetCodeTraining.Tests
+{
+    public class TwoSumSolutionTests
+    {
+        [Fact]
+        public void TwoSum_ArrayWith2Nums()
+        {
+            // Arrange
+            var solution = new TwoSum1.Solution();
+            int[] nums = new int[] { 2, 7, 11, 15 };
+            int target = 9;
+
+            // Act
+            int[] result = solution.TwoSum(nums, target);
+
+            // Assert
+            Assert.Equal(new int[] { 0, 1 }, result);
+
+        }
+    }
+}
---
+++ ScnSoft-Education/LeetCodeTraining/TwoSum1/TwoSumSolution.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCodeTraining.TwoSum1
+{
+    //https://leetcode.com/problems/two-sum/?source=submission-ac
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        return new int[] { i, j };
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}
