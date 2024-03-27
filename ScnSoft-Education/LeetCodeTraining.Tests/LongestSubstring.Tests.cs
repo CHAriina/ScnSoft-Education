aa584f32ff62af48897ac941a4f0940befe038ad
Arina Charniauskaya <acharniauskaya@scnsoft.com>
2024-03-27 06:35:10 +0300

    solution to the Longest Substring Without Repeating Characters problem and tests

---
+++ ScnSoft-Education/LeetCodeTraining.Tests/LongestSubstring.Tests.cs
@@ -0,0 +1,26 @@
+﻿using LeetCodeTraining.LongestSubstringWithoutRepeating3;
+
+namespace LeetCodeTraining.Tests
+{
+    public class LongestSubstring
+    {
+        [Fact]
+        public void LengthOfLongestSubstring_Test()
+        {
+            // Arrange
+            string input1 = "abcabcbb";
+            string input2 = "bbbbb";
+            string input3 = "pwwkew";
+
+            // Act
+            int result1 = Solution.LengthOfLongestSubstring(input1);
+            int result2 = Solution.LengthOfLongestSubstring(input2);
+            int result3 = Solution.LengthOfLongestSubstring(input3);
+
+            // Assert
+            Assert.Equal(3, result1); // "abc"
+            Assert.Equal(1, result2); // "b"
+            Assert.Equal(3, result3); // "wke"
+        }
+    }
+}
---
+++ ScnSoft-Education/LeetCodeTraining/LongestSubstringWithoutRepeating3/LongestSubstring.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCodeTraining.LongestSubstringWithoutRepeating3
+{
+    public class Solution
+    {
+        public static int LengthOfLongestSubstring(string s)
+        {
+            int maxLength = 0;
+            HashSet<char> set = [];
+            int start = 0, end = 0;
+
+            while (end < s.Length)
+            {
+                if (!set.Contains(s[end]))
+                {
+                    set.Add(s[end]);
+                    maxLength = Math.Max(maxLength, end - start + 1);
+                    end++;
+                }
+                else
+                {
+                    set.Remove(s[start]);
+                    start++;
+                }
+            }
+
+            return maxLength;
+        }
+    }
+}
