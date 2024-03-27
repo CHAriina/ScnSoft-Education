14acbf5273d4b0e4a02103d96566625910b73988
Arina Charniauskaya <acharniauskaya@scnsoft.com>
2024-03-27 15:49:50 +0300

    fixing code duplication in LeetCodeTraining.Tests/LongestSubstring.Tests.cs

--- ScnSoft-Education/LeetCodeTraining.Tests/LongestSubstring.Tests.cs
+++ ScnSoft-Education/LeetCodeTraining.Tests/LongestSubstring.Tests.cs
@@ -4,23 +4,17 @@ namespace LeetCodeTraining.Tests
 {
     public class LongestSubstring
     {
-        [Fact]
-        public void LengthOfLongestSubstring_Test()
+        [Theory]
+        [InlineData("abcabcbb", 3)] // "abc"
+        [InlineData("bbbbb", 1)] // "b"
+        [InlineData("pwwkew", 3)] // "wke"
+        public void LengthOfLongestSubstring_Test(string input, int expected)
         {
-            // Arrange
-            string input1 = "abcabcbb";
-            string input2 = "bbbbb";
-            string input3 = "pwwkew";
-
             // Act
-            int result1 = Solution.LengthOfLongestSubstring(input1);
-            int result2 = Solution.LengthOfLongestSubstring(input2);
-            int result3 = Solution.LengthOfLongestSubstring(input3);
+            int result = Solution.LengthOfLongestSubstring(input);
 
             // Assert
-            Assert.Equal(3, result1); // "abc"
-            Assert.Equal(1, result2); // "b"
-            Assert.Equal(3, result3); // "wke"
+            Assert.Equal(expected, result);
         }
     }
 }
