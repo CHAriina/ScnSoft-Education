0c88eba838e382a272c42099e3d397829a393b1d
Arina Charniauskaya <acharniauskaya@scnsoft.com>
2024-03-29 13:11:32 +0300

    ZigzagConversion problem solution and tests

---
+++ ScnSoft-Education/LeetCodeTraining.Tests/ZigzagConversion.Tests.cs
@@ -0,0 +1,16 @@
+﻿using LeetCodeTraining.ZigzagConversion5;
+namespace ZigzagConversionTests
+{
+    public class SolutionTests
+    {
+        [Theory]
+        [InlineData("PAYPALISHIRING", 3, "PAHNAPLSIIGYIR")]
+        [InlineData("PAYPALISHIRING", 4, "PINALSIGYAHRPI")]
+        [InlineData("A", 1, "A")]
+        public void Convert_ValidInput_ReturnsExpectedResult(string s, int numRows, string expectedResult)
+        {
+            // Arrange, Act, Assert
+            Assert.Equal(expectedResult, Solution.Convert(s, numRows));
+        }
+    }
+}
---
+++ ScnSoft-Education/LeetCodeTraining/ZigzagConversion5/ZigzagConversion.cs
@@ -0,0 +1,37 @@
+﻿
+namespace LeetCodeTraining.ZigzagConversion5
+{
+    public class Solution
+    {
+        public static string Convert(string s, int numRows)
+        {
+            if (numRows == 1 || s.Length <= numRows)
+            {
+                return s;
+            }
+
+            string[] rows = new string[numRows];
+            int currentRow = 0;
+            int direction = 1;
+
+            foreach (char c in s)
+            {
+                rows[currentRow] += c;
+                currentRow += direction;
+
+                if (currentRow == 0 || currentRow == numRows - 1)
+                {
+                    direction *= -1;
+                }
+            }
+
+            string result = "";
+            foreach (string row in rows)
+            {
+                result += row;
+            }
+
+            return result;
+        }
+    }
+} 
