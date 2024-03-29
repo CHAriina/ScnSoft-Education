dc6bbbf3869048cfd122b58a164ca252799c8157
Arina Charniauskaya <acharniauskaya@scnsoft.com>
2024-03-29 12:56:19 +0300

    solving the LongestPalindromicSubstring problem and tests

---
+++ ScnSoft-Education/LeetCodeTraining.Tests/Longest Palindromic Substring.Tests.cs
@@ -0,0 +1,26 @@
+﻿using LeetCodeTraining.LongestPalindromicSubstring4;
+using Xunit;
+
+namespace LongestPalindromicSubstringTests
+{
+    public class SolutionTests
+    {
+        [Theory]
+        [InlineData("babad", "bab")]
+        [InlineData("cbbd", "bb")]
+        [InlineData("", "")]
+        [InlineData("aaaaa", "aaaaa")]
+        public void LongestPalindrome_ValidInput_ReturnsExpectedResult(string input, string expectedResult)
+        {
+            // Arrange
+            var solution = new Solution();
+            string result;
+
+            // Act
+            result = solution.LongestPalindrome(input);
+
+            // Assert
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
