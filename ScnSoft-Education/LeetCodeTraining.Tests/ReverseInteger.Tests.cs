c48c88299c945574554529d48759159d0f660599
Arina Charniauskaya <acharniauskaya@scnsoft.com>
2024-04-01 07:01:05 +0300

    completing the ReverseInteger7 task

---
+++ ScnSoft-Education/LeetCodeTraining.Tests/ReverseInteger.Tests.cs
@@ -0,0 +1,19 @@
+﻿using LeetCodeTraining.ReverseInteger7;
+
+namespace LeetCodeTraining.Tests
+{
+    public class SolutionTests
+    {
+        [Theory]
+        [InlineData(123, 321)]
+        [InlineData(-123, -321)]
+        [InlineData(120, 21)]
+        [InlineData(1534236469, 0)] // Пример переполнения
+        public void TestReverse(int inputNumber, int expected)
+        {
+            Solution solution = new Solution();
+            int result = solution.Reverse(inputNumber);
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ ScnSoft-Education/LeetCodeTraining/ReverseInteger7/ReverseInteger.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCodeTraining.ReverseInteger7;
+public class Solution
+{
+    public int Reverse(int inputNumber)
+    {
+        int reversed = 0;
+
+        while (inputNumber != 0)
+        {
+            int digit = inputNumber % 10;
+            inputNumber /= 10;
+
+            if (reversed > Int32.MaxValue / 10 || reversed < Int32.MinValue / 10)
+            {
+                return 0;
+            }
+
+            reversed = reversed * 10 + digit;
+        }
+
+        return reversed;
+    }
+}
