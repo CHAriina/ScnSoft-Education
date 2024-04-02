22c4d3a5f3e2b55a21cad1fcaa158fff3287ab15
Arina Charniauskaya <acharniauskaya@scnsoft.com>
2024-04-02 07:52:50 +0300

    solving the String to Integer (atoi) problem

---
+++ ScnSoft-Education/LeetCodeTraining/StringToInteger8/StringToInteger.cs
@@ -0,0 +1,36 @@
+﻿namespace LeetCodeTraining.StringToInteger8
+{
+    public class Solution
+    {
+        public int MyAtoi(string inputString)
+        {
+            int index = 0;
+            int sign = 1;
+            int result = 0;
+
+            while (index < inputString.Length && inputString[index] == ' ')
+            {
+                index++;
+            }
+
+            if (index < inputString.Length && (inputString[index] == '+' || inputString[index] == '-'))
+            {
+                sign = inputString[index] == '-' ? -1 : 1;
+                index++;
+            }
+
+            while (index < inputString.Length && char.IsDigit(inputString[index]))
+            {
+                int digit = inputString[index] - '0';
+                if (result > (Int32.MaxValue - digit) / 10)
+                {
+                    return sign == 1 ? Int32.MaxValue : Int32.MinValue;
+                }
+                result = result * 10 + digit;
+                index++;
+            }
+
+            return sign * result;
+        }
+    }
+}
