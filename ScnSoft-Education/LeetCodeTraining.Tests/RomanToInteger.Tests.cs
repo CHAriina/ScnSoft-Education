085963d901aa283fb98eb30b29e30490f7c38477
Arina Charniauskaya <acharniauskaya@scnsoft.com>
2024-04-05 05:37:09 +0300

    solving the Roman To Integer problem (day 13, point 1)

---
+++ ScnSoft-Education/LeetCodeTraining.Tests/RomanToInteger.Tests.cs
@@ -0,0 +1,20 @@
+﻿using LeetCodeTraining.RomanTo_Integer12;
+
+namespace LeetCodeTraining.Tests.RomanTo_Integer
+{
+    public class SolutionTests
+    {
+        Solution solution = new Solution();
+        [Theory]
+        [InlineData("III", 3)]
+        [InlineData("IV", 4)]
+        [InlineData("IX", 9)]
+        [InlineData("LVIII", 58)]
+        [InlineData("MCMXCIV", 1994)]
+        public void TestRomanToInt(string s, int expected)
+        {
+            int result = solution.RomanToInt(s);
+            Assert.Equal(expected, result);
+        }
+    }
+}
