533eea23658f301f78204baa79edb6e266323ce7
Arina Charniauskaya <acharniauskaya@scnsoft.com>
2024-04-05 08:11:50 +0300

    solving the Longest Common Prefix problem (day 13, item 2)

---
+++ ScnSoft-Education/LeetCodeTraining/LongestCommonPrefix13/LongestCommonPrefix.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCodeTraining.LongestCommonPrefix13
+{
+    public class Solution
+    {
+        public string LongestCommonPrefix(string[] inputStrings)
+        {
+            if (inputStrings == null || inputStrings.Length == 0)
+            {
+                return "";
+            }
+
+            string currentPrefix = inputStrings[0];
+            for (int i = 1; i < inputStrings.Length; i++)
+            {
+                while (inputStrings[i].IndexOf(currentPrefix) != 0)
+                {
+                    currentPrefix = currentPrefix.Substring(0, currentPrefix.Length - 1);
+                    if (currentPrefix.Length == 0)
+                    {
+                        return "";
+                    }
+                }
+            }
+
+            return currentPrefix;
+        }
+    }
+}
