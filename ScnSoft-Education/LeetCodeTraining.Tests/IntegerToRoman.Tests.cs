bebc88dbc3038df469cecdea344741d83574c76c
Arina Charniauskaya <acharniauskaya@scnsoft.com>
2024-04-05 04:51:51 +0300

    solving the Integer To Roman problem (day 12, item 2)

---
+++ ScnSoft-Education/LeetCodeTraining.Tests/IntegerToRoman.Tests.cs
@@ -0,0 +1,23 @@
+﻿using LeetCodeTraining.IntegertToRoman11;
+namespace LeetCodeTraining.Tests.IntegertToRoman
+{
+    public class SolutionTestsIntegertToRomanv
+    {
+        [Theory]
+        [InlineData(3, "III")]
+        [InlineData(4, "IV")]
+        [InlineData(9, "IX")]
+        [InlineData(58, "LVIII")]
+        [InlineData(1994, "MCMXCIV")]
+        public void IntToRoman_WhenCalled_ReturnsCorrectRomanNumeral(int num, string expected)
+        {
+            // Arrange
+            var solution = new Solution();
+            // Act
+            string result = solution.IntToRoman(num);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
