d8e99c1764125d654231f02da3970c80ff816d4b
Arina Charniauskaya <acharniauskaya@scnsoft.com>
2024-04-08 04:34:03 +0300

    solving the letter-combinations-of-a-phone-number problem (day 15 first item)

---
+++ ScnSoft-Education/LeetCodeTraining/LetterCombinationsOfAPhoneNumber15/LetterCombinationsOfAPhoneNumber.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCodeTraining.LetterCombinationsOfAPhoneNumber15
+{
+    public class Solution
+    {
+        public IList<string> LetterCombinations(string digits)
+        {
+            var digitsLettersMap = new Dictionary<char, string>()
+            {
+                { '2', "abc" },
+                { '3', "def" },
+                { '4', "ghi" },
+                { '5', "jkl" },
+                { '6', "mno" },
+                { '7', "pqrs" },
+                { '8', "tuv" },
+                { '9', "wxyz" }
+            };
+
+            var res = new List<string>();
+            if (string.IsNullOrEmpty(digits))
+                return res;
+
+            res.Add(string.Empty);
+            for (var i = 0; i < digits.Length; i++)
+                res = res
+                    .SelectMany(x => digitsLettersMap[digits[i]].Select(y => x + y.ToString()))
+                    .ToList();
+
+            return res;
+
+        }
+    }
+}
