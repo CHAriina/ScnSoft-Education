7fdc0b68b5093f25dc5f17bc0f0e30376cec5967
Arina Charniauskaya <acharniauskaya@scnsoft.com>
2024-04-08 07:30:42 +0300

    Solving the FourSum problem (day 15, item 2)

---
+++ ScnSoft-Education/LeetCodeTraining.Tests/FourSum.Tests.cs
@@ -0,0 +1,26 @@
+﻿using LeetCodeTraining.FourSum16;
+
+namespace LeetCodeTraining.Tests.FourSum16
+{
+    public class SolutionTests
+    {
+        [Theory]
+        [InlineData(new int[] { 1, 0, -1, 0, -2, 2 }, 0, new int[] { -2, -1, 1, 2 }, new int[] { -2, 0, 0, 2 }, new int[] { -1, 0, 0, 1 })]
+        [InlineData(new int[] { 2, 2, 2, 2, 2 }, 8, new int[] { 2, 2, 2, 2 })]
+        public void FourSum_Test(int[] nums, int target, params int[][] expected)
+        {
+            // Arrange
+            Solution solution = new Solution();
+            // Act
+            IList<IList<int>> result = solution.FourSum(nums, target);
+
+            // Assert
+            Assert.Equal(expected.Length, result.Count);
+
+            foreach (var tuple in expected)
+            {
+                Assert.Contains(new List<int>(tuple), result);
+            }
+        }
+    }
+}
---
+++ ScnSoft-Education/LeetCodeTraining/FourSum16/FourSum.cs
@@ -0,0 +1,49 @@
+﻿namespace LeetCodeTraining.FourSum16
+{
+    public class Solution
+    {
+        public IList<IList<int>> FourSum(int[] nums, int target)
+        {
+            List<IList<int>> result = new List<IList<int>>();
+
+            Array.Sort(nums);
+
+            for (int firstNumIndex = 0; firstNumIndex < nums.Length - 3; firstNumIndex++)
+            {
+                if (firstNumIndex > 0 && nums[firstNumIndex] == nums[firstNumIndex - 1]) continue;
+                for (int secondNumIndex = firstNumIndex + 1; secondNumIndex < nums.Length - 2; secondNumIndex++)
+                {
+                    if (secondNumIndex > firstNumIndex + 1 && nums[secondNumIndex] == nums[secondNumIndex - 1]) continue;
+                    int leftPointer = secondNumIndex + 1;
+                    int rightPointer = nums.Length - 1;
+
+                    while (leftPointer < rightPointer)
+                    {
+                        long sum = (long)nums[firstNumIndex] + nums[secondNumIndex] + nums[leftPointer] + nums[rightPointer];
+
+                        if (sum == target)
+                        {
+                            result.Add(new List<int> { nums[firstNumIndex], nums[secondNumIndex], nums[leftPointer], nums[rightPointer] });
+
+                            while (leftPointer < rightPointer && nums[leftPointer] == nums[leftPointer + 1]) leftPointer++;
+                            while (leftPointer < rightPointer && nums[rightPointer] == nums[rightPointer - 1]) rightPointer--;
+
+                            leftPointer++;
+                            rightPointer--;
+                        }
+                        else if (sum < target)
+                        {
+                            leftPointer++;
+                        }
+                        else
+                        {
+                            rightPointer--;
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
