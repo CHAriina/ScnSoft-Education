a8aff56730bb672f08bf54da736b48200e843a29
Arina Charniauskaya <acharniauskaya@scnsoft.com>
2024-04-09 04:35:31 +0300

    code has been brought to a single style

--- ScnSoft-Education/LeetCodeTraining/FourSum16/FourSum.cs
+++ ScnSoft-Education/LeetCodeTraining/FourSum16/FourSum.cs
@@ -10,10 +10,16 @@
 
             for (int firstNumIndex = 0; firstNumIndex < nums.Length - 3; firstNumIndex++)
             {
-                if (firstNumIndex > 0 && nums[firstNumIndex] == nums[firstNumIndex - 1]) continue;
+                if (firstNumIndex > 0 && nums[firstNumIndex] == nums[firstNumIndex - 1])
+                {
+                    continue;
+                }
                 for (int secondNumIndex = firstNumIndex + 1; secondNumIndex < nums.Length - 2; secondNumIndex++)
                 {
-                    if (secondNumIndex > firstNumIndex + 1 && nums[secondNumIndex] == nums[secondNumIndex - 1]) continue;
+                    if (secondNumIndex > firstNumIndex + 1 && nums[secondNumIndex] == nums[secondNumIndex - 1])
+                    {
+                        continue;
+                    }
                     int leftPointer = secondNumIndex + 1;
                     int rightPointer = nums.Length - 1;
 
@@ -25,9 +31,14 @@
                         {
                             result.Add(new List<int> { nums[firstNumIndex], nums[secondNumIndex], nums[leftPointer], nums[rightPointer] });
 
-                            while (leftPointer < rightPointer && nums[leftPointer] == nums[leftPointer + 1]) leftPointer++;
-                            while (leftPointer < rightPointer && nums[rightPointer] == nums[rightPointer - 1]) rightPointer--;
-
+                            while (leftPointer < rightPointer && nums[leftPointer] == nums[leftPointer + 1])
+                            {
+                                leftPointer++;
+                            }
+                            while (leftPointer < rightPointer && nums[rightPointer] == nums[rightPointer - 1])
+                            {
+                                rightPointer--;
+                            }
                             leftPointer++;
                             rightPointer--;
                         }
