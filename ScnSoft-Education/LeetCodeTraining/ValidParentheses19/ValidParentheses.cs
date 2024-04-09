2454a274e82b2004a16a015cbe3dab08283ac55d
Arina Charniauskaya <acharniauskaya@scnsoft.com>
2024-04-09 06:49:14 +0300

    solving the problem remove-nth-node-from-end-of-list (day 16 point 2)

---
+++ ScnSoft-Education/LeetCodeTraining/ValidParentheses19/ValidParentheses.cs
@@ -0,0 +1,35 @@
+﻿namespace LeetCodeTraining.ValidParentheses19
+{
+    public class Solution
+    {
+        public bool IsValid(string inputString)
+        {
+            Stack<char> openBrackets = new Stack<char>();
+
+            foreach (char bracket in inputString)
+            {
+                if (bracket == '(' || bracket == '{' || bracket == '[')
+                {
+                    openBrackets.Push(bracket);
+                }
+                else
+                {
+                    if (openBrackets.Count == 0)
+                    {
+                        return false;
+                    }
+                    if (bracket == ')' && openBrackets.Peek() == '(' || bracket == '}' && openBrackets.Peek() == '{' || bracket == ']' && openBrackets.Peek() == '[')
+                    {
+                        openBrackets.Pop();
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return openBrackets.Count == 0;
+        }
+    }
+}
