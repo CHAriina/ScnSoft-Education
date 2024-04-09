e473f29c70056a5398b6590953f0b29f31fa460b
Arina Charniauskaya <acharniauskaya@scnsoft.com>
2024-04-09 04:48:58 +0300

    solving the ThreeSumClosest problem (day 14 point 2)

---
+++ ScnSoft-Education/LeetCodeTraining/ThreeSumClosest17/ThreeSumClosest.cs
@@ -0,0 +1,42 @@
+﻿namespace LeetCodeTraining.ThreeSumClosest17
+{
+    public class Solution
+    {
+        public int ThreeSumClosest(int[] nums, int target)
+        {
+            Array.Sort(nums);
+            int closestSum = int.MaxValue;
+            int minDiff = int.MaxValue;
+
+            for (int i = 0; i < nums.Length - 2; i++)
+            {
+                int firstNum = nums[i];
+                int leftPointer = i + 1;
+                int rightPointer = nums.Length - 1;
+
+                while (leftPointer < rightPointer)
+                {
+                    int currentSum = firstNum + nums[leftPointer] + nums[rightPointer];
+                    int currentDiff = Math.Abs(currentSum - target);
+
+                    if (currentDiff < minDiff)
+                    {
+                        minDiff = currentDiff;
+                        closestSum = currentSum;
+                    }
+
+                    if (currentSum < target)
+                    {
+                        leftPointer++;
+                    }
+                    else
+                    {
+                        rightPointer--;
+                    }
+                }
+            }
+
+            return closestSum;
+        }
+    }
+}
