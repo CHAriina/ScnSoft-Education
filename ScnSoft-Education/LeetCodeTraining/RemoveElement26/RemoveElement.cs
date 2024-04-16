32d23f5e48508014df773347acd370e37db78f4d
Arina Charniauskaya <acharniauskaya@scnsoft.com>
2024-04-16 07:08:52 +0300

    solving the 26 remove-element problem (day 20, item 1)

---
+++ ScnSoft-Education/LeetCodeTraining/RemoveElement26/RemoveElement.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCodeTraining.RemoveElement26
+{
+    public class Solution
+    {
+        public int RemoveElement(int[] nums, int val)
+        {
+            int nonValElementsCount = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != val)
+                {
+                    nums[nonValElementsCount] = nums[i];
+                    nonValElementsCount++;
+                }
+            }
+
+            return nonValElementsCount;
+        }
+    }
+}
