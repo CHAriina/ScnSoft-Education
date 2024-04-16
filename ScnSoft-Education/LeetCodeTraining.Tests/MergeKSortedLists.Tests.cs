6b05adc973a1c40f90abb9d0b4d8f32670d924c3
Arina Charniauskaya <acharniauskaya@scnsoft.com>
2024-04-16 06:44:54 +0300

    solving problem 22 merge-k-sorted-lists (day 18, item 1)

---
+++ ScnSoft-Education/LeetCodeTraining.Tests/MergeKSortedLists.Tests.cs
@@ -0,0 +1,48 @@
+﻿using LeetCodeTraining.MergeKSortedLists22;
+
+namespace LeetCodeTraining.Tests.MergeKSortedLists
+{
+    public class SolutionTests
+    {
+        [Theory]
+        [InlineData(new int[] { 1, 4, 5 }, new int[] { 1, 3, 4 }, new int[] { 2, 6 }, new int[] { 1, 1, 2, 3, 4, 4, 5, 6 })]
+        [InlineData(new int[] { }, new int[] { 1 }, new int[] { 2 }, new int[] { 1, 2 })]
+        [InlineData(new int[] { }, new int[] { }, new int[] { }, new int[] { })]
+        public void MergeKLists_ValidInput_ReturnsMergedSortedLinkedList(int[] input1, int[] input2, int[] input3, int[] expected)
+        {
+            // Arrange
+            ListNode list1 = CreateLinkedList(input1);
+            ListNode list2 = CreateLinkedList(input2);
+            ListNode list3 = CreateLinkedList(input3);
+
+            ListNode[] lists = new ListNode[] { list1, list2, list3 };
+            Solution solution = new Solution();
+
+            // Act
+            ListNode result = solution.MergeKLists(lists);
+
+            // Assert
+            List<int> actualValues = new List<int>();
+            ListNode current = result;
+            while (current != null)
+            {
+                actualValues.Add(current.val);
+                current = current.next;
+            }
+
+            Assert.Equal(expected, actualValues);
+        }
+
+        private ListNode CreateLinkedList(int[] values)
+        {
+            ListNode head = new ListNode();
+            ListNode current = head;
+            foreach (int val in values)
+            {
+                current.next = new ListNode(val);
+                current = current.next;
+            }
+            return head.next;
+        }
+    }
+}
---
+++ ScnSoft-Education/LeetCodeTraining/MergeKSortedLists22/MergeKSortedLists.cs
@@ -0,0 +1,48 @@
+﻿namespace LeetCodeTraining.MergeKSortedLists22
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public class Solution
+    {
+        public ListNode MergeKLists(ListNode[] lists)
+        {
+            if (lists == null || lists.Length == 0)
+            {
+                return null;
+            }
+
+            ListNode result = new ListNode();
+            ListNode current = result;
+            List<int> values = new List<int>();
+
+            foreach (var list in lists)
+            {
+                ListNode temp = list;
+                while (temp != null)
+                {
+                    values.Add(temp.val);
+                    temp = temp.next;
+                }
+            }
+
+            values.Sort();
+
+            foreach (var val in values)
+            {
+                current.next = new ListNode(val);
+                current = current.next;
+            }
+
+            return result.next;
+        }
+    }
+}
